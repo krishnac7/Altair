e27be02b18af241e3bbeeb841cd6da4513158c27
Phileas Fogg <phileas.fogg@mail.com>
2022-03-16 11:55:37 +0100

    Automatically detect the top_module from the filename

--- verilog/tools/yosys_aig.tcl
+++ verilog/tools/yosys_aig.tcl
@@ -1,5 +1,5 @@
 # Usage:
-# VLOG_FILE_NAME=.../dut.v yosys yosys_aig.tcl
+# TOP_LEVEL=dut VLOG_FILE_NAME=.../dut.v yosys yosys_aig.tcl
 
 proc pause {{message "Hit Enter to continue ==> "}} {
     puts -nonewline $message
@@ -9,32 +9,17 @@ proc pause {{message "Hit Enter to continue ==> "}} {
 
 yosys read_verilog $::env(VLOG_FILE_NAME)
 pause;
-if { $::env(TOPLEVEL) eq ""} {
-    yosys proc
-    yosys memory
-    yosys show
-    pause;
-    yosys synth
-    yosys flatten
-    yosys show
-    pause;
-    yosys abc -g AND,NAND,OR,NOR,ANDNOT,ORNOT # AIG
-    yosys clean
-    yosys show
-    pause;
-} else {
-    yosys proc
-    yosys memory
-    yosys show $::env(TOPLEVEL)
-    pause;
-    yosys synth -top $::env(TOPLEVEL)
-    yosys flatten
-    yosys show $::env(TOPLEVEL)
-    pause;
-    # Synthetize again but the flat design all together
-    yosys synth -top $::env(TOPLEVEL)
-    yosys abc -g AND,NAND,OR,NOR,ANDNOT,ORNOT # AIG
-    yosys clean
-    yosys show $::env(TOPLEVEL)
-    pause;
-}
+yosys proc
+yosys memory
+yosys show $::env(TOPLEVEL)
+pause;
+yosys synth -top $::env(TOPLEVEL)
+yosys flatten
+yosys show $::env(TOPLEVEL)
+pause;
+# Synthetize again but the flat design all together
+yosys synth -top $::env(TOPLEVEL)
+yosys abc -g AND,NAND,OR,NOR,ANDNOT,ORNOT # AIG
+yosys clean
+yosys show $::env(TOPLEVEL)
+pause;
