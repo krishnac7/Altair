c34b121b188a6d1a82b275742f57b833ff3d8be3
Phileas Fogg <phileas.fogg@mail.com>
2022-04-03 11:12:39 +0200

    Add script to install dependencies for Ubuntu/WSL

---
+++ verilog/ubuntu_dependencies.sh
@@ -0,0 +1,8 @@
+sudo apt install iverilog
+verilator
+git clone https://github.com/zachjs/sv2v.git
+cd sv2v/
+sudo apt install haskell-platform
+sudo cp bin/sv2v /usr/bin/
+sudo apt install opensta
+sudo apt install yosys
